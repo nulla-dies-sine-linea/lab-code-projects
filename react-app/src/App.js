4a9a7a8330e8d1452480b76cb1096d86fbc15bef
Светлана Кашина <sakashina@edu.hse.ru>
2021-05-06 16:21:04 +0500

    feat: 🎸 +useState

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,16 +2,27 @@ import React from 'react';
 import ToDoList from './ToDo/ToDoList';
 
 function App() {
-	const todos = [
+	const [todos, setTodos] = React.useState([
 		{ id: 1, completed: false, title: 'workout' },
-		{ id: 1, completed: false, title: 'water flowers' },
-		{ id: 1, completed: false, title: 'make lunch' },
-	];
+		{ id: 2, completed: true, title: 'water flowers' },
+		{ id: 3, completed: false, title: 'make lunch' },
+	]);
+
+	function toggleTodo(id) {
+		setTodos(
+			todos.map((todo) => {
+				if (todo.id === id) {
+					todo.completed = !todo.completed;
+				}
+				return todo;
+			})
+		);
+	}
 
 	return (
 		<div className='wrapper'>
 			<h1>To Do:</h1>
-			<ToDoList todos={todos} />
+			<ToDoList todos={todos} onToggle={toggleTodo} />
 		</div>
 	);
 }
--- react-app/src/ToDo/ToDoItem.js
+++ react-app/src/ToDo/ToDoItem.js
@@ -1,5 +1,50 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function ToDoItem({ todo }) {
-	return <li>{todo.title}</li>;
+const styles = {
+	li: {
+		display: 'flex',
+		justifyContent: 'space-between',
+		alighItems: 'center',
+		padding: '.5rem 1rem',
+		border: '1px solid #ccc',
+		borderRadius: '4px',
+		mardinButtom: '.5rem',
+	},
+	input: {
+		marginRight: '1rem',
+	},
+};
+
+function ToDoItem({ todo, index, onChange }) {
+	const classes = [];
+
+	if (todo.completed) {
+		classes.push('done');
+	}
+
+	return (
+		<li style={styles.li}>
+			<span className={classes.join(' ')}>
+				<input
+					type='checkbox'
+					checked={todo.completed}
+					style={styles.input}
+					onChange={() => onChange(todo.id)}
+				/>
+				<strong>{index + 1}</strong>
+				&nbsp;
+				{todo.title}
+			</span>
+			<button className='rm'>&times;</button>
+		</li>
+	);
 }
+
+ToDoItem.propTypes = {
+	todo: PropTypes.object.isRequired,
+	index: PropTypes.number,
+	onChange: PropTypes.func.isRequired,
+};
+
+export default ToDoItem;
