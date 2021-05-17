5c8a656e731685195bd1109792ea9b42361fadd5
Светлана Кашина <sakashina@edu.hse.ru>
2021-05-17 13:00:39 +0500

    feat: 🎸 footer added

---
+++ sandwich-app/src/components/Footer/Footer.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { createStyles, makeStyles, Theme, Typography } from '@material-ui/core';
+import classes from '*.module.css';
+
+const useStyles = makeStyles((theme: Theme) =>
+	createStyles({
+		root: {
+			display: 'flex',
+			flexDirection: 'column',
+			justifyContent: 'center',
+			alignItems: 'center',
+			backgroundColor: theme.palette.secondary.main,
+			color: 'white',
+			padding: '10px',
+		},
+	})
+);
+
+const Footer = (): JSX.Element => {
+	return (
+		<div className={classes.root}>
+			<Typography>All Rights Reserved</Typography>
+		</div>
+	);
+};
+
+export default Footer;
