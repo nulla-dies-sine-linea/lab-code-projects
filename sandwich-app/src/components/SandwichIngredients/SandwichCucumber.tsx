7c2bdfa6b07777987f561c33fa5cdb440c70d00a
Светлана Кашина <sakashina@edu.hse.ru>
2021-05-21 14:09:59 +0500

    feat: 🎸 sandwich ingredients added

---
+++ sandwich-app/src/components/SandwichIngredients/SandwichCucumber.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const SandwichCucumber = (): JSX.Element => {
+	return (
+		<div
+			style={{
+				height: '20px',
+				width: '580px',
+				backgroundColor: '#36b310',
+				border: '2px solid #1c7002',
+				borderRadius: '3px',
+			}}
+		/>
+	);
+};
+
+export default SandwichCucumber;
